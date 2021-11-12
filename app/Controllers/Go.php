f076e03a79d9b4262545995d713318fb5bb7560a
Alex Silva <rsalex@hotmail.com>
2021-11-11 19:01:14 -0600

    test

--- app/Controllers/Go.php
+++ app/Controllers/Go.php
@@ -4,25 +4,21 @@ namespace App\Controllers;
 
 class Go extends BaseController
 {
-    public function mapa()
-    {
+    public function mapa(){
         $this->data["coordenadas"] = model('LuminariaModel')->findAll();
         
         return plantilla("inicio/mapa", $this->data);
     }
 
-    public function consulta()
-    {
+    public function consulta(){
         return plantilla("inicio/consulta", $this->data);
     }
 
-    public function info()
-    {
+    public function info(){
         return plantilla("inicio/info", $this->data);
     }   
     
-    public function admin()
-    {
+    public function admin(){
         return plantilla("inicio/admin", $this->data);
     }       
 }
